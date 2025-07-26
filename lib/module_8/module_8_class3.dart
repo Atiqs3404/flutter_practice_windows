d9619d54023d03cf6e74a9cf4a90f59d1021c980
Atiqs3404 <atiqs7540@gmail.com>
2025-07-26 17:41:07 +0600

    module 9 class 1 all alerts

--- lib/module_8/module_8_class3.dart
+++ lib/module_8/module_8_class3.dart
@@ -12,65 +12,65 @@ class Module8Class3 extends StatelessWidget {
         backgroundColor: Colors.orange.withOpacity(0.75),
       ),
 
-      // body: GridView.count(
-      //     crossAxisCount: 3,
-      //   crossAxisSpacing: 10,
-      //   mainAxisSpacing: 10,
-      //   children: List.generate(10, (index) {
-      //     return Card(
-      //       elevation: 10,
-      //       color: Colors.orange.withOpacity(0.75),
-      //       child: Column(
-      //         mainAxisAlignment: MainAxisAlignment.center,
-      //         children: [
-      //           Icon(Icons.phone_android, size: 60, color: Colors.white,),
-      //           SizedBox(height: 5,),
-      //           Text("Cash Out",
-      //           style: TextStyle(
-      //             color: Colors.white,
-      //             fontSize: 25,
-      //           ),
-      //           ),
-      //         ],
-      //       ),
-      //   );
-      //   })
-      // ),
-
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      body: GridView.count(
           crossAxisCount: 3,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: 20,
-        itemBuilder: (context, index) {
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: List.generate(10, (index) {
           return Card(
             elevation: 10,
             color: Colors.orange.withOpacity(0.75),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                  Icons.phone_android,
-                  size: 60,
+                Icon(Icons.phone_android, size: 60, color: Colors.white,),
+                SizedBox(height: 5,),
+                Text("Cash Out",
+                style: TextStyle(
                   color: Colors.white,
+                  fontSize: 25,
                 ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  index.toString(),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                  ),
                 ),
               ],
             ),
-          );
-        },
+        );
+        })
       ),
+
+      // body: GridView.builder(
+      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //     crossAxisCount: 3,
+      //     crossAxisSpacing: 10,
+      //     mainAxisSpacing: 10,
+      //   ),
+      //   itemCount: 20,
+      //   itemBuilder: (context, index) {
+      //     return Card(
+      //       elevation: 10,
+      //       color: Colors.orange.withOpacity(0.75),
+      //       child: Column(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: [
+      //           Icon(
+      //             Icons.phone_android,
+      //             size: 60,
+      //             color: Colors.white,
+      //           ),
+      //           SizedBox(
+      //             height: 5,
+      //           ),
+      //           Text(
+      //             index.toString(),
+      //             style: TextStyle(
+      //               color: Colors.white,
+      //               fontSize: 25,
+      //             ),
+      //           ),
+      //         ],
+      //       ),
+      //     );
+      //   },
+      // ),
     );
   }
 }
