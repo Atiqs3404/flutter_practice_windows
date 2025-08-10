acf3ec137c43d7052d949e0d03530a91988c0743
Atiqs3404 <atiqs7540@gmail.com>
2025-08-10 11:37:02 +0600

    sum app full from mac

--- lib/module_11/class_2/calcButton.dart
+++ lib/module_11/class_2/calcButton.dart
@@ -2,29 +2,38 @@ import 'package:flutter/material.dart';
 
 class calculatorButton extends StatelessWidget {
 
-  const calculatorButton({
+  String text;
+  Color ? color;
+  final VoidCallback onClick;
+
+  calculatorButton({
     super.key,
+    required this.onClick,
+    this.color,
+    required this.text,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.all(24.0),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10)
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.all(24.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              backgroundColor: color ?? Colors.grey[800],
             ),
-            backgroundColor: Colors.grey[800],
+            onPressed: onClick, child: Text(text,
+          style: TextStyle(
+              fontSize: 25,
+              color: Colors.white
           ),
-          onPressed: (){}, child: Text("1",
-        style: TextStyle(
-            fontSize: 25,
-            color: Colors.white
-        ),
-      )
+        )
 
+        ),
       ),
     );
   }
--- lib/module_11/class_2/calculatorApp.dart
+++ lib/module_11/class_2/calculatorApp.dart
@@ -10,20 +10,155 @@ class Calculatorapp extends StatefulWidget {
 }
 
 class _CalculatorappState extends State<Calculatorapp> {
+
+  // Variables for calculator state
+  String _output = "0";
+  String _input = "0";
+  String _operator = "";
+  double _num1 = 0;
+  double _num2 = 0;
+
+// Function to handle button presses
+  void buttonPress(String buttonText) {
+    print(buttonText);
+
+    setState(() {
+      if(buttonText == "C"){
+        _output = "0";
+        _input = "0";
+        _operator = "";
+        _num1 = 0;
+        _num2 = 0;
+      }else if(buttonText == "="){
+        _num2 = double.parse(_input);
+        if(_operator == "+"){
+          _output = (_num1 + _num2).toString();
+        }else if(_operator == "-"){
+          _output = (_num1 - _num2).toString();
+        }else if(_operator == "*"){
+          _output = (_num1 * _num2).toString();
+        }else if(_operator == "÷"){
+          _output = _num2 != 0 ? (_num1 / _num2).toString() : "Error";
+        }
+      }else if(["+", "-", "*", "÷"].contains(buttonText)){
+        _num1 = double.parse(_input);
+        _operator = buttonText;
+        _input = "";
+      }else{
+        if(_input == "0"){
+           _input = buttonText;
+        }else{
+          _input += buttonText;
+        }
+        _output = _input;
+      }
+
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            calculatorButton()
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomRight,
+              padding: EdgeInsets.all(25),
+              child: Text(
+                _output,
+                style: TextStyle(fontSize: 50, color: Colors.white),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              calculatorButton(
+                onClick: () => buttonPress("7"),
+                text: '7',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("8"),
+                text: '8',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("9"),
+                text: '9',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("÷"),
+                text: '÷',
+                color: Colors.orange,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              calculatorButton(
+                onClick: () => buttonPress("4"),
+                text: '4',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("5"),
+                text: '5',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("6"),
+                text: '6',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("*"),
+                text: '*',
+                color: Colors.orange,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              calculatorButton(
+                onClick: () => buttonPress("1"),
+                text: '1',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("2"),
+                text: '2',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("3"),
+                text: '3',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("-"),
+                text: '-',
+                color: Colors.orange,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              calculatorButton(
+                onClick: () => buttonPress("C"),
+                text: 'C',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("0"),
+                text: '0',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("="),
+                text: '=',
+              ),
+              calculatorButton(
+                onClick: () => buttonPress("+"),
+                text: '+',
+                color: Colors.orange,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
-
-
