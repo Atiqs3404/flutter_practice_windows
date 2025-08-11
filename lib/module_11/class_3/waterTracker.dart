a47bdf7a16fb79ba89c6431ec0ad7fcbfe058b43
Atiqs3404 <atiqs7540@gmail.com>
2025-08-11 12:40:04 +0600

    Module 11 water tank with timer

--- lib/module_11/class_3/waterTracker.dart
+++ lib/module_11/class_3/waterTracker.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'addWaterBtn.dart';
@@ -10,44 +12,57 @@ class WaterTrackerApp extends StatefulWidget {
 }
 
 class _WaterTrackerAppState extends State<WaterTrackerApp> {
-
   int currentIntake = 0;
   final int goal = 5000;
+  Timer? _timer;
 
-  void waterAdd(int amount){
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (currentIntake > 0) {
+        setState(() {
+          currentIntake = (currentIntake - 1).clamp(0, goal);
+        });
+      }
+    });
+  }
+
+  void waterAdd(int amount) {
     setState(() {
       // Very important
       currentIntake = (currentIntake + amount).clamp(0, goal);
     });
   }
 
-  void resetTank(){
+  void resetTank() {
     setState(() {
       currentIntake = 0;
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-    double progress = (currentIntake/goal).clamp(0, 1);
+    double progress = (currentIntake / goal).clamp(0, 1);
 
     return Scaffold(
       backgroundColor: Colors.blue.shade50,
       appBar: AppBar(
-        title: Text("Water Tracker",
+          title: Text(
+        "Water Tracker",
         style: TextStyle(
           fontSize: 25,
           fontWeight: FontWeight.bold,
         ),
-        )
-      ),
-
+      )),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Container(
                 padding: EdgeInsets.all(80),
                 decoration: BoxDecoration(
@@ -56,69 +71,79 @@ class _WaterTrackerAppState extends State<WaterTrackerApp> {
                   // Box Shadow
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.blue.shade200,
-                      blurRadius: 10,
-                      spreadRadius: 2
-                    )
+                        color: Colors.blue.shade200,
+                        blurRadius: 10,
+                        spreadRadius: 2)
                   ],
                 ),
                 child: Column(
                   children: [
-                    Text("Today's Intank",
+                    Text(
+                      "Today's Intank",
                       style: TextStyle(
                         fontSize: 25,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 10,),
-                    Text("${currentIntake} LTR",
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "${currentIntake} LTR",
                       style: TextStyle(
                           fontSize: 30,
                           fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent
-                      ),
+                          color: Colors.blueAccent),
                     )
                   ],
                 ),
               ),
-          
-              SizedBox(height: 30,),
-          
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    height: 150,
-                    width: 150,
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.grey.shade500,
-                      color: Colors.blueAccent,
-                      strokeWidth: 10,
-                      value: progress, // (0.1 - 1) as percentage of the progress bar
-                    ),
-                  ),
-                  Text("${(progress * 100).toInt()}%",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold
-                  ),
-                  )
-                ]
+              SizedBox(
+                height: 30,
               ),
+              Stack(alignment: Alignment.center, children: [
+                Container(
+                  height: 150,
+                  width: 150,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.grey.shade500,
+                    color: Colors.blueAccent,
+                    strokeWidth: 10,
+                    value:
+                        progress, // (0.1 - 1) as percentage of the progress bar
+                  ),
+                ),
+                Text(
+                  "${(progress * 100).toInt()}%",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )
+              ]),
               Wrap(
                 // spacing: 10, // Horizontal spacing
                 // runSpacing: 10, // Vertical spacing
                 children: [
-                  addWaterBtn(amount: 200, onClick: () => waterAdd(200),),
-                  addWaterBtn(amount: 500, onClick: () => waterAdd(500), icon: Icons.water,),
-                  addWaterBtn(amount: 1000, onClick: () => waterAdd(1000), icon: Icons.dangerous_outlined,),
+                  addWaterBtn(
+                    amount: 200,
+                    onClick: () => waterAdd(200),
+                  ),
+                  addWaterBtn(
+                    amount: 500,
+                    onClick: () => waterAdd(500),
+                    icon: Icons.water,
+                  ),
+                  addWaterBtn(
+                    amount: 1000,
+                    onClick: () => waterAdd(1000),
+                    icon: Icons.dangerous_outlined,
+                  ),
                 ],
               ),
               SizedBox(
                 width: double.infinity,
                 child: Padding(
                   padding: const EdgeInsets.all(15.0),
-                  child: ElevatedButton(onPressed: () => resetTank(), child: Text("reset")),
+                  child: ElevatedButton(
+                      onPressed: () => resetTank(), child: Text("reset")),
                 ),
               ),
             ],
@@ -128,5 +153,3 @@ class _WaterTrackerAppState extends State<WaterTrackerApp> {
     );
   }
 }
-
-
